55e79e209407db8d6f6e3ef659f5476078b2986e
Abdulazeez Jimoh <abdulazeezojimoh@gmail.com>
2023-04-04 14:30:32 +0100

    Drop profile view

--- farmlyfe/lib/app/bindings/profile.dart
+++
@@ -1,9 +0,0 @@
-import 'package:farmlyfe/app/controllers/profile.dart';
-import 'package:get/get.dart';
-
-class ProfileBindings extends Bindings {
-  @override
-  void dependencies() {
-    Get.lazyPut(() => ProfileController());
-  }
-}
